f98f7a6495813455fdbb6ffa6d5cdfe90607bbd8
Rakesh-Krd <rakeshkrd7877@gmail.com>
2026-05-10 20:59:28 +0530

    Replace AutoTokenizer and AutoModel with BERT equivalents

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import (
-    AutoTokenizer,
-    AutoModelForSequenceClassification
+    BertTokenizer,
+    BertForSequenceClassification
 )
 
 import torch
